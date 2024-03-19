5d4d8b26e132ded8270dcc69288e453ce855c6ee
Nikita Nikandrov <nnikandroff@yandex.ru>
2024-03-19 23:07:11 +0300

    Initial Commit

---
+++ Moments2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moments2.0
+//
+//  Created by Nikita Nikandrov on 19.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
